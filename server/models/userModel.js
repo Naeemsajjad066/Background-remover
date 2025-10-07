52b098fc1dd0dd94ce6a23a66b392b806fe62053
Naeem Sajjad <naeem9924066@gmail.com>
2025-10-07 19:30:09 +0500

    made changes

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     clerkId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    photo: { type: String, required: true },
+    photo: { type: String, required : false },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     creditBalance: { type: Number, default: 5 },
